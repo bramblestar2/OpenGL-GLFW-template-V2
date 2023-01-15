9a672e77cdef0029d4c4c9c1e62eff7b983f1667
Jay Feather2000 <thegals4321@gmail.com>
2023-01-14 22:20:44 -0600

    a

--- OpenGL GLFW template V2/OpenGL GLFW template V2.cpp
+++ OpenGL GLFW template V2/OpenGL GLFW template V2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "Headers/Graphics/Window.h"
 #include "Headers/Graphics/View.h"
+#include "Headers/Graphics/Shader.h"
 
 void error_callback(int error, const char* msg)
 {
@@ -16,13 +17,43 @@ int main()
 
     glfwSetErrorCallback(error_callback);
 
-    jf::ContextSettings settings(24, 8, 0, 1, 2);
-    jf::Window window(500, 300, "Title", settings);
+    jf::ContextSettings settings(24, 8, 0, 3, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    jf::Window window(300, 300, "Title", settings);
 
     if (glewInit() != GLEW_OK)
         return -1;
 
-    jf::View view({500, 300}, {0,0});
+    jf::View view({ 500, 300 }, { 0,0 });
+
+    float square[] =
+    {
+        -0.5f, -0.5f, 0.0f,   1.f, 0.f, 0.f,
+        -0.5f, 0.5f,  0.0f,   0.f, 0.f, 1.f,
+        0.5f, 0.5f,   0.0f,   0.f, 1.f, 0.f,
+
+        -0.5f, -0.5f, 0.0f,   1.f, 0.f, 0.f,
+        0.5f, 0.5f,  0.0f,    0.f, 1.f, 0.f,
+        0.5f, -0.5f,   0.0f,  0.f, 0.f, 1.f,
+    };
+
+    unsigned int VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    // bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(square), square, GL_STATIC_DRAW);
+
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (const void*)0);
+    glEnableVertexAttribArray(0);
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (const void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    Shader shader("2dShader.vert", "2dShader.frag");
 
     while (window.isOpen())
     {
@@ -36,27 +67,20 @@ int main()
             }
         }
 
-        double xpos, ypos;
-        glfwGetCursorPos(window.getWindow(), &xpos, &ypos);
+        window.clear(.2,.3,.3);
 
-        printf(std::string(std::to_string(xpos) + " - " + std::to_string(ypos) + "\n").c_str());
 
-        window.clear();
+        shader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 6);
 
-        glBegin(GL_QUADS);
-        glColor3f(1, 1, 1);
-        glVertex2f(0,0);
-        glColor3f(1, 1, 0);
-        glVertex2f(100,0);
-        glColor3f(1, 0, 0);
-        glVertex2f(100,100);
-        glColor3f(0, 1, 0);
-        glVertex2f(0,100);
-        glEnd();
 
-        view.update();
         window.display();
     }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 
     glfwTerminate();
+    return 0;
 }
---
+++ OpenGL GLFW template V2/Source Files/Graphics/Shader.cpp
@@ -0,0 +1,1 @@
+#include "../../Headers/Graphics/Shader.h"
